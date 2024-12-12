a784d718037bed925136959dc45bc0ce83be5367
DS0708 <jonum12312@gmail.com>
2024-12-13 05:06:35 +0900

    remove filefix

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -4,32 +4,29 @@ const fileController = require('../controllers/fileController');
 const auth = require('../middleware/auth');
 const { validateFileUpload, validateUploadComplete, validateFileId } = require('../middleware/validator');
 
-// API 엔드포인트 정의
-const FILE_SERVICE_PREFIX = '/file-service';
-
 // 파일 업로드 초기화 (Pre-signed URL 발급)
-router.post(`${FILE_SERVICE_PREFIX}/upload/init`,
+router.post('/upload/init',
   auth,
   validateFileUpload,
   fileController.initiateUpload
 );
 
 // 파일 업로드 완료 확인
-router.post(`${FILE_SERVICE_PREFIX}/upload/complete/:uploadId`,
+router.post('/upload/complete/:uploadId',
   auth,
   validateUploadComplete,
   fileController.completeUpload
 );
 
 // 파일 정보 조회
-router.get(`${FILE_SERVICE_PREFIX}/files/:fileId`,
+router.get('/files/:fileId',
   auth,
   validateFileId,
   fileController.getFileInfo
 );
 
 // 파일 삭제
-router.delete(`${FILE_SERVICE_PREFIX}/files/:fileId`,
+router.delete('/files/:fileId',
   auth,
   validateFileId,
   fileController.deleteFile
